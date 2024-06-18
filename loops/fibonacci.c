594fdc1488106fa3e5a1f1848e2c60602962b755
Navneet <148934112+Navneet0094@users.noreply.github.com>
2024-06-19 01:31:12 +0530

    Update fibonacci.c

--- loops/fibonacci.c
+++ loops/fibonacci.c
@@ -6,7 +6,7 @@ void main()
     int n;
     printf("enter number ");
     scanf("%d",&n);
-    int a=1,b=1;
+    int a=0,b=1;
     printf("%d %d ",a,b);
     int sum;//sum =1 not =0 bcz.if we ask for2nd it will give 0
     for(int i=1; i<=n-2;i++)//n-2 lia kunki a or b ko phle se nhi maan ra isilie 2 barkam loop chlayege
